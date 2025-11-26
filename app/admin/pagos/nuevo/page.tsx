0acd8b04abe975398d2312623c20828ef5e46969
matiasrodriguez0411 <matiasrodriguez0411@gmail.com>
2025-11-26 16:22:08 +0000

    1

--- app/admin/pagos/nuevo/page.tsx
+++ app/admin/pagos/nuevo/page.tsx
@@ -416,7 +416,7 @@ export default function NuevoPagoPage() {
                       rows={3}
                       value={formData.notas}
                       onChange={handleChange}
-                      className="flex h-20 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+                      className="flex h-20 w-full rounded-md border border-input bg-backgrond px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
                       placeholder="Agrega cualquier nota adicional sobre el pago"
                     />
                   </div>
